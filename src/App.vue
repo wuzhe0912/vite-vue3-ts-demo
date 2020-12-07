2b9dddef4e5d16cc347bbfc9c9033650606c3174
Pitt Wu <kgb00128@gmail.com>
2020-12-07 22:28:54 +0800

    feat：ref 寫法

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
 .wrap
   HelloWorld(msg="Hello Vue 3.0 + Vite")
+  TodoList
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+.wrap
+  h3 {{ name }}
+  div 年齡：{{ age }}
+  button(@click="add") 年齡遞增+1
+  div 計算年齡x2的結果： {{ double }}
+</template>
+
+<script>
+import { computed, ref } from 'vue'
+
+export default {
+  name: 'todolist',
+
+  setup() {
+    const name = ref('Pitt');
+    const age = ref(32);
+    const double = computed(() => {
+      return age.value * 2
+    })
+    function add() {
+      age.value += 1
+    }
+
+    return { name, age, add, double };
+  }
+}
+</script>
