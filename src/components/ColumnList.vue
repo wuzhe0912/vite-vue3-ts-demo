342580faaeef554c3382b5380b6d5599de07dfc6
pitt.wu <pitt.wu@jh-tech.tw>
2020-12-17 16:30:59 +0800

    feat：add component

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.wrap column list
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
---
+++ src/components/Dropdown.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+.wrap dropdown
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
